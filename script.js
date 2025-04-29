b45143c3170e7407672dea3a6913b8128433fb5f
Jeff Wheeler <jeff.wheeler@ucalgary.ca>
2025-04-29 10:31:51 -0600

    Create etch-a-sketch web page.

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const grid = document.querySelector('div');
-const gridSize = 16;
+const body = document.querySelector('body');
+const header = document.createElement('header');
+body.appendChild(header);
+const grid = document.createElement('div');
+body.appendChild(grid);
+const prompt = document.createElement('p');
+prompt.textContent = 'Enter a grid size:';
+header.appendChild(prompt);
+const gridEntry = document.createElement('input');
+header.appendChild(gridEntry);
+const button = document.createElement('button');
+button.textContent = 'Reset'
+header.appendChild(button)
+const errorMessage = document.createElement('p');
+header.appendChild(errorMessage);
+
+let gridSize = 16;
 
 let createColumn = (grid, size) => {
     const column = document.createElement('div');
@@ -13,9 +28,39 @@ let createColumn = (grid, size) => {
 }
 
 let createGrid = (grid, size) => {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild)
+    }
+
     for (let i = 0; i < size; i++){
         createColumn(grid, size);
     }
 }
 
+let pixelInitializer = () => {
+    const pixels = document.querySelectorAll('.square, .hoveredsquare');
+    // pixels.classList.remove('hoveredsquare');
+    // pixels.classList.add('square');
+
+    pixels.forEach(div => div.addEventListener('mouseover', (e) => {
+        div.classList.remove('square');
+        div.classList.add('hoveredsquare');
+    }));
+}
+
 createGrid(grid, gridSize);
+pixelInitializer();
+
+button.addEventListener('click', (e) => {
+    newSize = gridEntry.value;
+    console.log(newSize);
+    if (newSize > 0 && newSize <= 100){
+        errorMessage.textContent = '';
+        createGrid(grid, newSize);
+        pixelInitializer();
+    } else {
+        errorMessage.textContent = 'Max grid size is 100.';
+    }
+})
+
+
